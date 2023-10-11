1f9c65565fe881a9d7a9ab4499fc1aaa1c805310
Victor-Okpukpan <vokpukpan@gmail.com>
2023-10-11 16:29:24 +0100

    added api edits

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,20 @@
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { NextRequest, NextResponse } from "next/server";
 
 export const authOptions = {
-    providers: [
-        GoogleProvider({
-          clientId: process.env.GOOGLE_ID!,
-          clientSecret: process.env.GOOGLE_SECRET!
-        }),
-    ],
-}
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!
+    })
+  ]
+};
 
 const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST };
+// Explicitly specify the types for handler
+const GET = (req: NextRequest, res: NextResponse) => handler(req, res);
+const POST = (req: NextRequest, res: NextResponse) => handler(req, res);
+
+export { GET, POST };
